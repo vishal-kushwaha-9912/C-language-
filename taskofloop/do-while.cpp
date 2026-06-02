b3c56e88c3e1e42a60802b7a4323f8dba07e0e66
vishal-kushwaha-9912 <vishalk.ushwa9912@gmail.com>
2026-06-03 03:33:57 +0530

    Add all C++ project files

--- taskofloop/do-while.cpp
+++ taskofloop/do-while.cpp
@@ -1,24 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-
-int cups;
-cout<<"enter the number of tea cups :"<<endl;
-cin>>cups;
-
-cout<<"your " <<cups<< " tea cups are ready"<<endl;
-
-string order;
-do{
-    cout<<"do you want to order more tea cups(yes/no)"<<endl;
-    cin>>order;
-
-}
-while(  order != "no" && order != "NO"   );
+int main()
 {
-    cout<<"thanks for your order"<<endl;
-}
 
-return 0;
+    int cups;
+    cout << "enter the number of tea cups :" << endl;
+    cin >> cups;
+    string order;
 
+    do
+    {
+        cout << "pouring more tea.... (if buying enter 'yes' and not 'no')" << endl;
+        cin >> order;
+
+    } while (order != "no" && order != "NO");
+    {
+        cout << "sorry you have no order of tea  " << endl;
+    }
+
+    return 0;
 }
+//                                                **  NOTE ** 
+
+// if you write this 
+//  while (order == "no" || order == "NO"); this is wrong its give you ''YES and NO'' both if the user enter the yes or no any one option.
+// 
