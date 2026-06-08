63abe73563aed5042c16ec7192f049b6defec419
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-09 00:23:42 +0530

    add some files

--- question/Eight.cpp
+++ question/Eight.cpp
@@ -8,11 +8,17 @@
 using namespace std;
 int main()
 {
+    int count = 0;
     int num;
-    cout<<"Enter the nuber that you want to  count the digit :"<<endl;
+    cout<<"Enter the number that you want to count the digits of: "<<endl;
     cin>>num;
 
-    
+    while (num!=0)  // we will keep dividing the number by 10 until it becomes 0, and count how many times we can do this
+    {
+        num=num/10; // this will remove the last digit of the number, and we will keep doing this until the number becomes 0
+        count++;
+    }
+    cout<<"The number of digits in the given number is: "<<count<<endl;
     
 
     return 0;
--- question/ten.cpp
+++ question/ten.cpp
@@ -2,3 +2,37 @@
 // Problem
 
 // Take a number and reverse it.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int rev;
+    int count;
+    int digit;
+    int num;
+    cout << "Enter the number you want to reverse it : " << endl;
+    cin >> num;
+
+    while (num != 0)
+    {
+        num = num / 10;
+        count++;
+
+        cout << "" << count;
+    }
+
+    while (num != 0)
+    {
+
+        int power =10*count;
+
+        num = num/power;
+        digit = num % 10;
+        rev = rev * 10 + digit;
+        cout << "Reversed number is: " << rev << endl;
+    }
+
+    return 0;
+}
