a3bf252e907b1d5b67697abf45344f518a47423c
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-18 01:00:16 +0530

    pattern

---
+++ patterncode/pattern1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std ;
+int main(){
+
+for (int i= 0; i<=5; i++){
+ cout<<" X "<<endl;
+     for (int j=1; j<=3; j++){
+        cout<<" Y "<<endl;
+     }
+  
+}
+  
+return 0;
+}
+
+//OUTPUT : 
+// > X    <
+//  Y 
+//  Y 
+//  Y 
+// > X      <
+//  Y 
+//  Y 
+//  Y 
+//> X      <
+//  Y 
+//  Y 
+//  Y 
+// > X      <
+//  Y 
+//  Y 
+//  Y 
+// > X      <
+//  Y 
+//  Y 
+//  Y 
