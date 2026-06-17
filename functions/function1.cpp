0abaa7ed5cb044fc6c14320b1d06d2217edec930
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-18 02:59:23 +0530

    functiobasics

--- functions/function1.cpp
+++ functions/function1.cpp
@@ -3,22 +3,24 @@
 // A function is a block of code that does a specific task.
 // First we define the function by writing what it should do, then we call the function whenever we want to execute that task.
 
- Two Main Parts
 
-1. Function Definition
 
-This is where we write what the function does.
+//  Two Main Parts
 
-void sayHello()
-{
-    cout << "Hello!" << endl;
-}
+// 1. Function Definition
 
-2. Function Call
+// This is where we write what the function does.
 
-This is where we tell the computer to run the function.
+// void sayHello()
+// {
+//     cout << "Hello!" << endl;
+// }
 
-sayHello();
+// 2. Function Call
+
+// This is where we tell the computer to run the function.
+
+// sayHello();
 
 
 
---
+++ functions/functionbasics.cpp
@@ -0,0 +1,33 @@
+//FUNCTION :
+// In the first step, we store the work (instructions) inside a function. 
+// In the second step, we call the function whenever we need that work to be done.
+
+
+Imagine Your Mom Says:
+
+👩 "Vishal, whenever I say 'Make Tea', do these steps:"
+//steps to make tea
+1 Boil water
+2 Add tea leaves
+3 Add milk
+4 Serve tea
+
+You don't do the steps immediately. You just store the instructions.
+
+This is like creating a function.
+
+void makeTea()
+{
+    // steps to make tea
+}
+Then Later...
+
+Your mom says:
+
+👉 "Make Tea!"
+
+Now you actually perform those steps.
+
+This is like calling the function.
+
+makeTea(); //calling the function.
