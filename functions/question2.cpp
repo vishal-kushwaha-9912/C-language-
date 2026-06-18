d1a18a6db3e0629bab50889904847591f7ece083
Your Name <vishalk.ushwa9912@gmail.com>
2026-06-19 01:44:23 +0530

    functions

--- functions/question2.cpp
+++ functions/question2.cpp
@@ -23,22 +23,72 @@
 
 
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
 // Question 2: Function with Return Value (Beginner)
 // Write a function called add() that takes two integers as parameters and returns their sum. 
 //Then create a main program that calls this function and displays the result
 
+
+//ANSWER
+
+
+// #include<iostream>
+// using namespace std;
+
+// int add (int num1,int num2){
+//  return num1+num2;
+// }
+
+// int main(){  //functioon call
+//    int result =add(3,2);
+
+//    cout<<"the sum  : "<<result<<endl;
+//    return 0;
+
+// }
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// Question 3: Multiple Parameters and Return (Intermediate)
+// Write a function called calculateAverage() that takes three numbers as parameters and returns their average.
+ //Test it with different values and display the results.
+
+//  #include<iostream>
+//  using namespace std;
+
+// double calculateAverage(int num1,int num2,int num3){
+//     return (num1+num2+num3)/3.0;
+
+//  }
+// int main(){
+//     double average =calculateAverage(0,9,11);
+
+// cout<<"the average is :"<<average<<endl;
+
+//     return 0;
+// }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+// Question 4:   Function with Local Variables (Intermediate)
+
+
+// Write a function called countVowels() that takes a string as parameter 
+// and returns the count of vowels (a, e, i, o, u) in that string. Then use it to count vowels in at least 2 different sentences.
+
+
+//ANSWER :
+
 #include<iostream>
 using namespace std;
-
-int add (int num1,int num2){
- return num1+num2;
-}
-
-int main(){  //functioon call
-   int result =add(3,2);
-
-   cout<<"the sum  : "<<result<<endl;
-   return 0;
-
-    return 0;
+countVowels(string){
+    
 }
